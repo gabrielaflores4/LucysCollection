31b2c6322cbea84bdcf600be291292f3db52e214
monse2023 <monsebarillas28@gmail.com>
2025-03-29 23:00:39 -0600

    Conexion Postgresql

--- C_Datos/Conexion.cs
+++ C_Datos/Conexion.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,22 @@ using System.Threading.Tasks;
 
 namespace C_Datos
 {
-    internal class Conexion
+    public class Conexion
     {
+        private static string cadenaConexion = "Host=localhost;Port=5432;Username=postgres;Password=postgress123;Database=lucysdb";
+
+        public static NpgsqlConnection ObtenerConexion()
+        {
+            try
+            {
+                NpgsqlConnection conexion = new NpgsqlConnection(cadenaConexion);
+                conexion.Open();
+                return conexion;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al conectar con PostgreSQL: " + ex.Message);
+            }
+        }
     }
 }
